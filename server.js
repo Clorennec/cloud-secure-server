fd344105ab23e33d00508f4de5f3bb8cab37073c
Clorennec <louis.clorennec@bnli.fr>
2024-02-01 17:42:06 +0100

    server changes

--- server.js
+++ server.js
@@ -1,8 +1,52 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const app = express();
-const port = 3000;
+const port = 4000;
+
+const CLIENT_ID = '05b8fef3db2a5766094f';
+const CLIENT_SECRET = '37eb40dbeeeba6a32c893c78a3dd34218b3bfb48';
+
+app.use(cors);
+app.use(bodyParser.json);
+
+app.get('/getAccessToken', async function (req, res) {
+  req.query.code;
+  const params = "?client_id=" + CLIENT_ID + "&client_secret=" +CLIENT_SECRET + "&code=" + req.query.code;
+  await fetch('https://github.com/login/oauth/access_token' + params, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json"
+    }
+  }).then((response) => {
+    return response.json();
+  }).then((data) => {
+    res.json(data);
+  })
+})
+
+app.get('/getUserData', async function (req, res) {
+  req.get('Authorization')
+  await fetch('https://api.github.com/user', {
+    method: "GET",
+    headers: {
+      "Authorization": req.get("Authorization")
+    }
+  }).then((response) => {
+    return response.json();
+  }).then((data) => {
+    console.log(data);
+    res.json(data);
+  })
+})
+
+app.get('/test', (req, res) => {
+  res.status(200).send('test recu');
+})
 
 // Endpoint pour le déploiement
 app.post('/deploy', (req, res) => {
