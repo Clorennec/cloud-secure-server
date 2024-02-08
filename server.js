fa094608794cfface6e68b78e89666e3ef824413
Clorennec <louis.clorennec@bnli.fr>
2024-02-09 00:15:10 +0100

    cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 
 app.use(
 	cors({
-		origin: "http://192.168.5.10:3000",
+		origin: "http://localhost:3000",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
